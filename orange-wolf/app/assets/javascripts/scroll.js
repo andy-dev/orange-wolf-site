c12bad44a5a50607845cd0a1aef75c051575a1c7
andy-dev <andres.macedo@gmail.com>
2015-09-04 20:34:30 -0700

    add controller and model for projects, modify index

--- orange-wolf/app/assets/javascripts/scroll.js
+++ orange-wolf/app/assets/javascripts/scroll.js
@@ -6,29 +6,29 @@
     });
   });
 
-  $(window).scroll(function(){
-    var wScroll = $(this).scrollTop();
+  // $(window).scroll(function(){
+  //   var wScroll = $(this).scrollTop();
 
-    $(".name-box").css({
-      'transform' : 'translate(0px, '+ wScroll/3  + '%)'
-    });
+  //   $(".name-box").css({
+  //     'transform' : 'translate(0px, '+ wScroll/3  + '%)'
+  //   });
 
-    $(".quote-box").css({
-      'transform' : 'translate(0px, '+ wScroll + '%)'
-    });
+  //   $(".quote-box").css({
+  //     'transform' : 'translate(0px, '+ wScroll + '%)'
+  //   });
 
-    if(wScroll>650){
-      $(".quote-box").text('');
-    }
+  //   if(wScroll>650){
+  //     $(".quote-box").text('');
+  //   }
 
-    if(wScroll > $('.projects').offset().top - ($(window).height() / 1.2)) {
-      $('.projects figure').each(function(i){
-        setTimeout(function(){
-          $('.projects figure').eq(i).addClass('is-showing');
-        }, (700 * (Math.exp(i * 0.14))) - 700);
-      });
-    }
-  });
+  //   if(wScroll > $('.projects').offset().top - ($(window).height() / 1.2)) {
+  //     $('.projects figure').each(function(i){
+  //       setTimeout(function(){
+  //         $('.projects figure').eq(i).addClass('is-showing');
+  //       }, (700 * (Math.exp(i * 0.14))) - 700);
+  //     });
+  //   }
+  // });
 
 
 
