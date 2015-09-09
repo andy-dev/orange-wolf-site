c1e985b96611e8a29bbab59d67078b6ff24439c3
andy-dev <andres.macedo@gmail.com>
2015-09-09 08:10:45 -0700

    called eventProp

--- app/assets/javascripts/playground.js
+++ app/assets/javascripts/playground.js
@@ -5,6 +5,7 @@ $(function(){
   showEverywhere("I love JS!!");
   console.log(player.weapons);
   mybouncingSquare();
+  eventProp();
 })
 //-------------------
 // event bubbling
@@ -26,7 +27,9 @@ var eventProp = function (){
         evt.cancelBubble = true // else use this one
       }
       this.style.borderColor = "red";
-      alert(this.getAttribute("id") + " border color changed")
+      // alert(this.getAttribute("id") + " border color changed")
+      alert( " border color changed")
+
     }
   }
 };
