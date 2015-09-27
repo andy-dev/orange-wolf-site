a6431735a2a490f9cb8e24cc692e408ecef9b874
andy-dev <andres.macedo@gmail.com>
2015-09-26 19:28:09 -0700

    add bubble sort example in playground

--- app/assets/javascripts/playground.js
+++ app/assets/javascripts/playground.js
@@ -305,3 +305,25 @@ var Dom = (function (){
 
 // notes on doc ready
 // ajax calls do not need to be on doc ready dont use it unless you need it
+
+
+//bubble sort
+var mySorting = function bubbleSort(array){
+  var arrayLength = array.length;
+  var i = 0;
+
+  for (i;i<arrayLength;i++) {
+    var n = 0;
+    for (n; n<arrayLength -1; n++) {
+      if (array[n+1] < array[n]) {
+        var switchedNumber = array[n];
+        array[n] = array[n+1];
+        array[n+1] = switchedNumber;
+      };
+    };
+  }
+  return array;
+}
+
+var myArray = [1,3,5,2,4,3,6,8,10,9,7]
+console.log(mySorting(myArray))
